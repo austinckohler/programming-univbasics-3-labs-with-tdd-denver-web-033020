fe691b18db49a2a2f25b81e7642e27392a359ac7
Austin Kohler <austinckohler@gmail.com>
2020-01-20 16:06:04 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,10 @@
-# Add your variables here
+describe "./calculator.rb" do
+first_number = 20
+
+  it "contains a local variable called first_number that is assigned to a number" do
+    first_number = get_variable_from_file('./calculator.rb', "5")
+
+    expect(first_number).to be_an(Integer).or be_a(Float)
+  end
+  
+ 
