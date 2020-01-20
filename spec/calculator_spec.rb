7b1878e5cc2482c5f23a26c7cbfeeeabbceae350
Austin Kohler <austinckohler@gmail.com>
2020-01-20 16:02:38 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -7,15 +7,13 @@ describe "./calculator.rb" do
 
     expect(first_number).to be_an(Integer).or be_a(Float)
   end
-
-  it "contains a local variable called second_number that is assigned to a number that isn't 0" do
+ it "contains a local variable called second_number that is assigned to a number that isn't 0" do
     second_number = get_variable_from_file('./calculator.rb', "second_number")
 
     expect(second_number).to be_an(Integer).or be_a(Float)
     expect(second_number).not_to equal(0)
   end
-
-  it "contains a local variable called sum that is assigned to the result of adding first_number and second_number" do
+    it "contains a local variable called sum that is assigned to the result of adding first_number and second_number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
     second_number = get_variable_from_file('./calculator.rb', "second_number")
     sum = get_variable_from_file('./calculator.rb', "sum")
@@ -47,3 +45,4 @@ describe "./calculator.rb" do
     expect(quotient).to eq(first_number/second_number)
   end
 end
+  
